86927d9d6f3809484dd3c5555fd2944665c1bd53
Arthur Correll <arthurcorrell@gmail.com>
2025-04-22 20:03:51 -0600

    Client and Debugger Service nodes. Accessible as executables from the cli

---
+++ src/behavior_tree/behavior_tree/service_member_function.py
@@ -0,0 +1,35 @@
+import sys
+import rclpy
+import rclpy.node as rcl
+
+import behavior_tree.BehaviorTree as BehaviorTree
+
+# dummy service to use for debugging; the BT only calls services, not hosts them
+class BTDebugService(rcl.Node, BehaviorTree.Action):
+    def __init__(self, name:str, srv_obj, srv_name: str, blackboard={}):
+        rcl.Node.__init__(self, name)
+        BehaviorTree.Action.__init__(self, name, self.action)
+        self.srv = self.create_service(srv_obj, srv_name, self.action)
+
+        self.blackboard = blackboard
+    
+    # service functions are not defined in behavior tree
+    def action(self, request, response):
+        response.success = True
+        response.message = 'bruh'
+        self.get_logger().info('Service requested')
+        return response
+
+def main(args=None):
+    from example_interfaces.srv import SetBool
+    rclpy.init(args=args)
+    
+    bt_service = BTDebugService('return_bool', SetBool, 'set_boolean')
+
+    rclpy.spin(bt_service)
+
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
